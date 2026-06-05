120de57d6331fff2b114273f38d823e1a799f8f9
Your Name <itismepranish@gmail.com>
2026-06-05 21:15:59 +0545

    chore(main): Authorization func done

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -3,21 +3,43 @@ package middleware
 import (
 	"errors"
 	"net/http"
+
 	"github.com/Nika-commits/Go_api/api"
+
 	// "github.com/Nika-commits/Go_api/internal/tools"
 	log "github.com/sirupsen/logrus"
 )
 
-var UnauthorizedError = errors.New("Invalid credentials")
+var UnAuthorizedError = errors.New("Invalid credentials")
 
 func Authorization(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r * http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var username string = r.URL.Query().Get("username")
 		var token = r.Header.Get("Authorization")
 		var err error
 
-		if username =="" || token == "" {
-			
+		if username == "" || token == "" {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
 		}
+
+		var database *tools.DatabaseInterface
+		database, err = tools.NewDatabase()
+
+		if err != nil {
+			api.InternalErrorHandler(w)
+			return
+		}
+
+		var loginDetails *tools.LoginDetails
+		loginDetails = (*&database).GetUserLoginDetails(username)
+
+		if loginDetails == nil || (token != (*loginDetails).AuthToken) {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
 }
